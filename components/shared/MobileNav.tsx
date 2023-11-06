d6435a0c718d96f37466889ec42098527de7bbec
shubham <singlas106@gmail.com>
2023-11-06 12:44:31 +0530

    global search

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetClose,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import { sidebarLinks } from "@/constants/Constants";
+import { usePathname } from "next/navigation";
+
+const NavContent = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="flex flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => {
+        const isActive =
+          pathname === item.route ||
+          (pathname.includes(item.route) && item.route.length > 1);
+        return (
+          <SheetClose asChild key={item.label}>
+            <Link
+              href={item.route}
+              className={`flex items-center gap-4 bg-transparent p-4 ${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              }`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={` ${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={` ${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/images/site-logo.svg"
+            alt="logo"
+            height={23}
+            width={23}
+          />
+
+          <h2 className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            dev <span className="text-primary-500">Overflow</span>
+          </h2>
+        </Link>
+        <SheetClose asChild>
+          <NavContent />
+        </SheetClose>
+
+        <SignedOut>
+          <div className="flex flex-col gap-3">
+            <SheetClose>
+              <Link href="/sign-in">
+                <Button className="small-medium btn-secondary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <span className="primary-text-gradient">Log In</span>
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose>
+              <Link href="/sign-up">
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
+        </SignedOut>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
