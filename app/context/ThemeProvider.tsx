b6a7ef0dccb683dd2655f24964b294462eff9e21
shubham <singlas106@gmail.com>
2023-11-06 10:04:49 +0530

    changes in theme

--- app/context/ThemeProvider.tsx
+++ app/context/ThemeProvider.tsx
@@ -12,12 +12,16 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [mode, setMode] = useState("");
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
@@ -36,7 +40,7 @@ export const useTheme = () => {
   const context = useContext(ThemeContext);
 
   if (context === undefined) {
-    throw new Error("useTheme must be used within a theme pROVIDER");
+    throw new Error("useTheme must be used within a Theme Provider");
   }
   return context;
 };
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,6 +2,7 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
@@ -19,7 +20,7 @@ const Navbar = () => {
       </Link>
       globalSearch
       <div className="flex-between gap-5">
-        theme
+        <Theme />
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
