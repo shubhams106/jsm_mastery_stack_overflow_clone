8cf9000b8a4c7cf358fa3e5966934b8b69b655b1
shubham <singlas106@gmail.com>
2023-11-06 21:17:09 +0530

    filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,38 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question" className=" flex justify-end max-sm:w-full">
+          <Button className=" primary-gradient px-4 py-3 text-light-900 ">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="flex-between mt-11 gap-5 max-sm:flex max-sm:flex-col ">
+        <LocalSearchBar
+          route="/"
+          placeholder="Search for questions"
+          imgSrc="/assets/icons/search.svg"
+          iconPosition="left"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+
+      <HomeFilters />
+    </>
+  );
 }
