b9ae35610b91a749796bd74cfaca0278fc967559
shubham <singlas106@gmail.com>
2023-11-06 22:49:36 +0530

    no results page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,41 @@
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import RenderTag from "@/components/shared/RenderTag";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import link from "next/link";
 import Link from "next/link";
+
+const questions = [
+  // {
+  //   _id: 1,
+  //   title: "tall depend alive height oldest grown afresh personal instant g",
+  //   tags: [
+  //     { _id: 1, name: "python" },
+  //     { _id: 2, name: "js" },
+  //   ],
+  //   auther: "Shubham singla",
+  //   upvotes: 20,
+  //   views: 2,
+  //   answers: 43,
+  //   createdAt: "2021-09-01T12:00:00.000Z",
+  // },
+  // {
+  //   _id: 2,
+  //   title: "tall depend alive height oldest grown afresh personal instant g",
+  //   tags: [
+  //     { _id: 1, name: "abc" },
+  //     { _id: 2, name: "next" },
+  //   ],
+  //   auther: "Shubham",
+  //   upvotes: 20,
+  //   views: 2,
+  //   answers: 483,
+  //   createdAt: "2021-09-01T12:00:00.000Z",
+  // },
+];
 
 export default function Home() {
   return (
@@ -33,6 +65,37 @@ export default function Home() {
       </div>
 
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions?.length > 0 ? (
+          questions.map((question) => {
+            return "QuestionCard";
+          })
+        ) : (
+          <NoResult
+            title="There's no Question to show"
+            description="Be the first to break the silence. Ask a question and kickstart the discussion. Your query could be the next big thing we can learn from.Get involved!"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
+}
+{
+  /* <>
+<h3 className="" key={question._id}>
+  {question.title}
+</h3>
+<div className="">
+  {question.tags.map((tag) => {
+    return (
+      <div className="flex gap-2" key={tag._id}>
+        <RenderTag _id={tag._id} name={tag.name} />
+      </div>
+    );
+  })}
+</div>
+</> */
 }
